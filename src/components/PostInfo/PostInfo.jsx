a96882cc1378db84c975d414d26f9b95b90db257
Naomi <naomidie1311@gmail.com>
2025-09-30 19:57:07 +0200

    StaticListND

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,30 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post, title, body, user, comments }) => {
+  // Support both post object and individual props for flexibility
+  const postTitle = post ? post.title : title;
+  const postBody = post ? post.body : body;
+  const postUser = post ? post.user : user;
+  const postComments = post ? post.comments : comments;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{postTitle}</h3>
+        <p>
+          {' Posted by  '}
+          {postUser ? <UserInfo user={postUser} /> : 'Unknown User'}
+        </p>
+      </div>
+      <p className="PostInfo__body">{postBody}</p>
+      <hr />
+
+      {postComments && postComments.length > 0 ? (
+        <CommentList comments={postComments} />
+      ) : (
+        <b data-cy="NoCommentsMessage" />
+      )}
+    </div>
+  );
+};
