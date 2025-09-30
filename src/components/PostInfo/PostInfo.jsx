2a9019983396d9c7a73ea5847bcda5fad2d480c8
Naomi <naomidie1311@gmail.com>
2025-09-30 20:14:00 +0200

    StaticList_ND_002

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,30 +1,23 @@
+import './PostInfo.scss';
 import { UserInfo } from '../UserInfo/UserInfo';
 import { CommentList } from '../CommentList/CommentList';
 
-export const PostInfo = ({ post, title, body, user, comments }) => {
-  // Support both post object and individual props for flexibility
-  const postTitle = post ? post.title : title;
-  const postBody = post ? post.body : body;
-  const postUser = post ? post.user : user;
-  const postComments = post ? post.comments : comments;
-
-  return (
-    <div className="PostInfo">
-      <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{postTitle}</h3>
-        <p>
-          {' Posted by  '}
-          {postUser ? <UserInfo user={postUser} /> : 'Unknown User'}
-        </p>
-      </div>
-      <p className="PostInfo__body">{postBody}</p>
-      <hr />
-
-      {postComments && postComments.length > 0 ? (
-        <CommentList comments={postComments} />
-      ) : (
-        <b data-cy="NoCommentsMessage" />
-      )}
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <p>
+        {' Posted by  '}
+        {post.user ? <UserInfo user={post.user} /> : 'Unknown User'}
+      </p>
     </div>
-  );
-};
+    <p className="PostInfo__body">{post.body}</p>
+    <hr />
+
+    {post.comments && post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage" />
+    )}
+  </div>
+);
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,15 +1,10 @@
+import './PostList.scss';
 import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
-      <PostInfo
-        key={post.id}
-        title={post.title}
-        body={post.body}
-        user={post.user}
-        comments={post.comments}
-      />
+      <PostInfo key={post.id} post={post} />
     ))}
   </div>
 );
