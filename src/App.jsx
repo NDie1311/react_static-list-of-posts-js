63d350d929c85afc73b386fe1341a3440d9b71ad
Naomi <naomidie1311@gmail.com>
2025-10-01 20:11:52 +0200

    Fix_StaticListofPosts

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import usersFromServer from './api/users.json';
 
 import { PostList } from './components/PostList/PostList';
 
-const getUserById = userId =>
-  usersFromServer.find(user => user.id === userId) || null;
+export const App = () => {
+  const getUserById = userId =>
+    usersFromServer.find(user => user.id === userId) || null;
 
-const getCommentsByPostId = postId =>
-  commentsFromServer.filter(comment => comment.postId === postId);
+  const getCommentsByPostId = postId =>
+    commentsFromServer.filter(comment => comment.postId === postId);
 
-export const posts = postsFromServer.map(post => ({
-  ...post,
-  user: getUserById(post.userId),
-  comments: getCommentsByPostId(post.id),
-}));
+  const posts = postsFromServer.map(post => ({
+    ...post,
+    user: getUserById(post.userId),
+    comments: getCommentsByPostId(post.id),
+  }));
 
-export const App = () => (
-  <section className="App">
-    <h1 className="App__title">Static list of posts</h1>
-    <PostList posts={posts} />
-  </section>
-);
+  return (
+    <section className="App">
+      <h1 className="App__title">Static list of posts</h1>
+      <PostList posts={posts} />
+    </section>
+  );
+};
